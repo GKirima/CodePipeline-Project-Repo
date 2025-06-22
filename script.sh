e5572ec94eb927796a46dba269b4dbce821fde0a
George Kirima <kirimageorge@gmail.com>
2025-06-22 10:17:31 -0700

    Update the files

--- script.sh
+++ script.sh
@@ -2,3 +2,8 @@ echo Hello from script.sh
 echo Hello World!
 echo Script exection completed
 echo Uploading artifacts to s3....
+Step 1: System info
+Step 2: Current user
+George
+Step 3: Showing config.txt contents
+cat /home/ec2-user/app/config.txt
